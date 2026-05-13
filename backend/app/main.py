b4caac340d6c3bddfde3e8faa34c84d074aa9aa7
QuantaBoy <quantachanger@gmail.com>
2026-05-14 01:24:33 +0530

    Connected Authentication Router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI 
 from app.models import Base
 from app.database import engine 
+from app.routes import auth 
 
 Base.metadata.create_all(bind=engine)
  
 app = FastAPI()
+
+app.include_router(auth.router)
 
 @app.get("/")
 def home():
