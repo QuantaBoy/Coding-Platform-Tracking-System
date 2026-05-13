774380687d2b92471b46b47b2d5f00ee5f28f820
QuantaBoy <quantachanger@gmail.com>
2026-05-13 20:47:54 +0530

    User Table Created

---
+++ backend/app/models.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String 
+from app.database import Base
+
+class Users(Base):
+    __tablename__ = 'Users'
+    id = Column(Integer, primary_key = True, index = True)
+    name = Column(String,nullable = False)
+    email = Column(String, nullable = False)
+    password = Column(String, nullable = False)
