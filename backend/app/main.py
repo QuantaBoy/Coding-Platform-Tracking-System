d9a3b0476ad8917b95ea42482646e51560049a54
QuantaBoy <quantachanger@gmail.com>
2026-05-14 09:48:20 +0530

    Added Profile Router

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI 
 from app.models import Base
 from app.database import engine 
-from app.routes import auth 
-
+from app.routes import auth, profile
 Base.metadata.create_all(bind=engine)
  
 app = FastAPI()
 
 app.include_router(auth.router)
+app.include_router(profile.router)
 
 @app.get("/")
 def home():
