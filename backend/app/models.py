3195be9f77fa9cde565fccfcf4edde6f599a072b
QuantaBoy <quantachanger@gmail.com>
2026-05-14 06:31:38 +0530

    Table Name Correction

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String 
 from app.database import Base
 
-class Users(Base):
+class User(Base):
     __tablename__ = 'Users'
     id = Column(Integer, primary_key = True, index = True)
     name = Column(String,nullable = False)
