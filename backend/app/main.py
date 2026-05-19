1a12578d71e99b92db67454a13f0249160f58357
QuantaBoy <quantachanger@gmail.com>
2026-05-19 21:23:03 +0530

    Included Dashboard

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI 
 from app.models import Base
 from app.database import engine 
-from app.routes import auth, profile
+from app.routes import auth, profile, dashboard
 Base.metadata.create_all(bind=engine)
  
 app = FastAPI()
 
 app.include_router(auth.router)
 app.include_router(profile.router)
+app.include_router(dashboard.router)
 
 @app.get("/")
 def home():
