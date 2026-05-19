53e0b298f01e40a192df78545667bc2f4ac0a598
QuantaBoy <quantachanger@gmail.com>
2026-05-19 21:23:59 +0530

    Completed Codeforce Integration

--- backend/app/routes/dashboard.py
+++ backend/app/routes/dashboard.py
@@ -5,9 +5,25 @@ from app.services.code_forces import get_user_info, get_rating_history, get_subm
 
 router = APIRouter()
 
-@router.get("/dashboard/{username}")
-
-def get_dashboard(user_id :int):
-
+@router.get("/dashboard/{roll_number}")
+def get_dashboard(roll_number: str):
     db = SessionLocal()
     
+    profile = db.query(CompleteProfile).filter(CompleteProfile.roll_number == roll_number).first()
+
+    db.close()
+
+    if not profile:
+        return {"message":"Profile not Found"}
+
+    codeforces_username = profile.codeforces_username
+
+    user_info = get_user_info(codeforces_username)
+    rating_history = get_rating_history(codeforces_username)
+    submissions = get_submission(codeforces_username)
+
+    return {
+        "codeforces_info" : user_info,
+        "rating_history" : rating_history,
+        "submissions" : submissions
+    }
